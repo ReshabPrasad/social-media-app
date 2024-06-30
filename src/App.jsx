ca323a471bb279de4e2702fd34dce5d94820d1cb
ReshabPrasad <153843058+ReshabPrasad@users.noreply.github.com>
2024-07-01 00:37:50 +0530

    Post card added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import PostCard from './components/Post'
+import SocialApp from './components/SocialApp'
+
+
+function App() {
+
+  return (
+    <>
+      <SocialApp/>
+      <PostCard/>
+    </>
+  )
+}
+
+export default App
